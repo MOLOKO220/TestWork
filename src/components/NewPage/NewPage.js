e317c9b88ef729b3d3393e0c5861cfff08ad3503
MOLOKO220 <441yan@gmail.com>
2022-02-07 18:48:43 +0200

    test5

--- src/components/NewPage/NewPage.js
+++ src/components/NewPage/NewPage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function NewPage(props) {
   return (
     <div className="wrapp">
-      <a href="testWork">Home</a>
+      <a href="../testWork">Home</a>
     </div>
   );
 }
